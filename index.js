f593f9f7513428122ca0a4ed2614c0088b2aa563
Serhii Savchuk <serhii.savchuk@altran.com>
2022-02-14 12:35:32 +0200

    remove debugger

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { info, warn, error } = require("./utils/logger");
 
 info("Hello comrade, this is info color text");
-debugger;
 warn("Hello comrade, this is warn color text");
 error("Hello comrade, this is error color text");
