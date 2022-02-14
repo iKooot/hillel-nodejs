7f2956127bc82b280b9475f85488b4a93085387c
Serhii Savchuk <serhii.savchuk@altran.com>
2022-02-14 12:12:59 +0200

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const { info, warn, error } = require("./utils/logger");
 
-info("Hello comrade, this is info color text");
-warn("Hello comrade, this is warn color text");
-error("Hello comrade, this is error color text");
