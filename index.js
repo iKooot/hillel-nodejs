32ca268b7adef5986e853af954c952013336b14c
Serhii Savchuk <serhii.savchuk@altran.com>
2022-02-23 10:53:39 +0200

    squash 8 commits
    start work with task
    
    added log file
    
    start work with task
    
    remove logic
    
    added logic
    
    remove files
    
    added files

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const yargs = require("yargs/yargs");
-const { seek, seekEmitter } = require("./utils/FileSeeker");
+const { seek } = require("./utils/fileSeeker");
+const { info, error, warn } = require("./utils/logger");
+const { logger } = require('./utils/writerLoggs')
 
 const argv = yargs(process.argv).argv;
 
-seekEmitter.emit("start", "Started project");
+seek(argv.fileName, argv.fileDir)
+  .on("success", (payload) => {
+    const { message, path } = payload;
 
-seek(argv.fileName, argv.fileDir);
+    const alertMessage = `${message}\n-------\nPath: ${path}`;
+
+    info(alertMessage);
+
+    if (argv.verbose) {
+      logger( 'success', payload)
+    }
+  })
+  .on("data", (payload) => {
+    warn(`\n------- Content of file -------\n`);
+    warn(payload);
+
+    if (argv.verbose) {
+      logger( 'data', payload)
+    }
+  })
+  .on("error", (payload) => {
+    error(payload);
+
+    if (argv.verbose) {
+      logger( 'error', payload)
+    }
+  });
