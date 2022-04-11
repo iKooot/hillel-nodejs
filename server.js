f13d88a4f6e72415ef7b3b5a2a74055ef37c1ce8
Serhii Savchuk <serhii.savchuk@altran.com>
2022-04-11 13:19:44 +0300

    added browser reloding

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const routes = require("./routes");
 const bodyParser = require('body-parser')
+const livereload = require('livereload')
+const connectLiveReload = require("connect-livereload");
 
 const HOST_NAME = process.env.HOST_NAME ?? "localhost";
 const PORT = process.env.PORT ?? 3004;
 
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 const app = express();
+
+app.use(connectLiveReload());
 
 app.use(bodyParser());
 
